fe51f72a0f6e781c280fc092c8bf7f1511ebeeea
scott <scottysora@gmail.com>
2024-04-29 15:02:22 -0400

    complete mini project temp converter

---
+++ celsiusConvert.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function celsiusToFahrenheit() {
+  const celsius = parseFloat(document.getElementById("celsius").value);
+
+  const fahrenheit = (celsius * 9) / 5 + 32;
+
+  document.getElementById(
+    "result"
+  ).innerHTML = `${celsius}°C is ${fahrenheit.toFixed(2)}°F`;
+}
